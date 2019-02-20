97ab6f588e3df01573ee7dd4a8ad4a1581164ef3
Sérgio Souza Costa <sergio.costa@ufma.br>
2019-02-19 22:10:13 -0300

    update

--- vagrant/instfuseki.pp
+++ vagrant/instfuseki.pp
@@ -42,38 +42,76 @@ service { "fuseki":
   start   => "/etc/init.d/fuseki start",
   stop    => "/etc/init.d/fuseki stop",
   restart =>  "/etc/init.d/fuseki restart",
-  require => Exec['app_ln']
+  require => Exec['app_ln'],
+   subscribe => File["/usr/local/apache-jena-fuseki-3.10.0/run/configuration/config.ttl","/usr/local/apache-jena-fuseki-3.10.0/run/shiro.ini"],
 }
-
-file { 'copia_configura':
-    ensure => file,
-    path   => '/usr/local/apache-jena-fuseki-3.10.0/run/configuration/config.ttl',
-    source => "/vagrant/config.ttl",
-    #require => Service["fuseki"],
-     notify => Service["fuseki"]
-  }
 
 
 
 #configurando
 
-exec {"configura":
-    command => "/bin/sed -i 's/\/$\/\*\* = localhostFilter/#\/$\/\*\* = localhostFilter/g' /usr/local/apache-jena-fuseki-3.10.0/run/shiro.ini",
-    require => Service["fuseki"],
-    unless => "/bin/grep  '#/\$/\*\* = localhostFilter' /usr/local/apache-jena-fuseki-3.10.0/run/shiro.ini",
-    #notify => Service["fuseki"]
+file { 'cria_diretorio_base':
+    ensure => directory,
+    path   => '/usr/local/apache-jena-fuseki-3.10.0/run',
+    require => Exec['fuseki_untar']  
+  }
+
+
+file { 'cria_diretorio':
+    ensure => directory,
+    path   => '/usr/local/apache-jena-fuseki-3.10.0/run/configuration/',
+    require => File['cria_diretorio_base']
+  }
+
+file { 'copia_configura':
+    ensure => file,
+    path   => '/usr/local/apache-jena-fuseki-3.10.0/run/configuration/config.ttl',
+    source => "/vagrant/config.ttl",
+    require => File['cria_diretorio'],
+    notify => Service["fuseki"]
+}
+
+file { 'shiroini':
+    ensure => file,
+    path   => '/usr/local/apache-jena-fuseki-3.10.0/run/shiro.ini',
+    source => "/vagrant/shiro.ini",
+    require => File['cria_diretorio'],
+    notify => Service["fuseki"]
 }
 
 
-exec {"import_rdf":
-    command => "/usr/local/apache-jena-fuseki-3.10.0/bin/s-put http://localhost:3030/ds/data default /vagrant/rdf/cursos.rdf",
-    require => File['copia_configura']
-}
 
 
 /*
 
+#todo: fazer esse comando ser executado só apos o restart
+exec {"import_rdf":
+    command => "/usr/local/apache-jena-fuseki-3.10.0/bin/s-put http://localhost:3030/ds/data default /vagrant/rdf/cursos.rdf",
+   # require => Exec['start_fuseki']
+}
 
+
+exec {"stop_fuseki":
+    command => "/etc/init.d/fuseki stop",
+    require => [Service["fuseki"], File[copia_configura]]
+}
+
+exec {"start_fuseki":
+    command => "/etc/init.d/fuseki start",
+    require => Exec["stop_fuseki"]
+}
+
+
+exec {"configura":
+    command => "/bin/sed -i 's/\/$\/\*\* = localhostFilter/#\/$\/\*\* = localhostFilter/g' /usr/local/apache-jena-fuseki-3.10.0/run/shiro.ini",
+    require => File['cria_diretorio'],
+    unless => "/bin/grep  '#/\$/\*\* = localhostFilter' /usr/local/apache-jena-fuseki-3.10.0/run/shiro.ini",
+    notify => Service["fuseki"]
+}
+*/
+
+
+/*
 exec {"fuseki_start":
     environment => [ "FUSEKI_HOME=/usr/local/apache-jena-fuseki-3.10.0/" ] ,
     #command => "/usr/local/apache-jena-fuseki-3.10.0/fuseki-server --update --mem /ds",
@@ -85,13 +123,4 @@ exec {"fuseki_home":
     require => Exec['fuseki_untar'],
     notify => Service["fuseki"]
 }
-
-
-
 */
-
-
-
-
-
-
