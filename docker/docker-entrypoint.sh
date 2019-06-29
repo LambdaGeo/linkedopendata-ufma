e1f13f56dc7756784ec8a5612b28a564aa155aa8
sergio costa <prof.sergio.costa@gmail.com>
2019-06-29 06:20:30 -0300

    testes

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -54,8 +54,9 @@ done
 echo "importando"
 #curl -X GET "https://dados-ufma.herokuapp.com/api/v01/docente/?subunidade=1396" -H  "accept: application/xml" > /jena-fuseki/rdf/docentes.rdf
 #/jena-fuseki/bin/s-put http://localhost:$PORT/ds/data default /jena-fuseki/rdf/cursos.rdf
-/jena-fuseki/bin/s-put http://localhost:$PORT/ds/data default /jena-fuseki/rdf/docente.rdf
 /jena-fuseki/bin/s-put http://localhost:$PORT/ds/data default /jena-fuseki/rdf/subunidade.rdf
+while : ; do sleep 10s; done
+/jena-fuseki/bin/s-put http://localhost:$PORT/ds/data default /jena-fuseki/rdf/docente.rdf
 echo "importado"
 
 
